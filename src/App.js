37140e3cb05a1cda9782f01019a8a4de7a04b51b
angie <amaticorena103@hotmail.com>
2022-07-08 20:32:17 -0400

    Building routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from "./components/NavBar";
+import Patients from "./components/Patients";
+import { useState, useEffect} from "react";
+
 
 function App() {
+
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/patients")
+    .then(r => r.json())
+    .then(pets => setPets(pets))
+  }, []);
+
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="App">
+      <NavBar />
+      <h1>Hello World</h1>
+      <Routes>
+        <Route path="/allpets" element={<Patients />} />
+        <Route path="/" element ={<Home/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Patients.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Patients() {
+    return (
+        <h1>patient</h1>
+    )
+};
+
+export default Patients
