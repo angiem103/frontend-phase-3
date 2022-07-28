c4566211a0a693ed86898bf5f8b507ff013304e1
angie <amaticorena103@hotmail.com>
2022-07-28 14:02:50 -0400

    changing constant name pet to patient

--- src/App.js
+++ src/App.js
@@ -1,53 +1,54 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
-import NavBar from "./components/NavBar";
-import Pets from "./components/Pets";
-import Home from "./components/Home";
-import PetDetails from "./components/PetDetails";
-import NewPet from "./components/NewPet";
-import EditPet from "./components/EditPet";
-import Vets from "./components/Vets";
 import { useState, useEffect} from "react";
+import NavBar from "./components/NavBar";
+import Patients from "./components/Patients";
+import Home from "./components/Home";
+import PatientDetails from "./components/PatientDetails";
+import NewPatient from "./components/NewPatient";
+import EditPatient from "./components/EditPatient";
+import Vets from "./components/Vets";
+
 
 
 
 function App() {
 
-  const [pets, setPets] = useState([]);
+  const [patients, setPatients] = useState([]);
 
 
   useEffect(() => {
     fetch("http://localhost:9292/patients")
     .then(r => r.json())
-    .then(pets => setPets(pets))
+    .then(patients => setPatients(patients))
   }, []);
 
 
-  function addPatient(newPet){
-    setPets([...pets,newPet])
-  }
+  function addPatient(newPatient){
+    setPets([...patients,newPatient])
+  };
 
-  function deletePatient(patient){
-    const updatedPatients = pets.filter(pet => pet.id !== patient.id)
+  function deletePatient(deletedPatient){
+    const updatedPatients = patients.filter(patient => patient.id !== deletedPatient.id)
     setPets(updatedPatients)
-  }
+  };
 
-  function handleEdit(editedPet){
-    const unchangedPatients = pets.filter(pet => pet.id !== editedPet.id)
-    setPets([unchangedPatients,editedPet])
-  }
+  function handleEdit(editedPatient){
+    const unchangedPatients = patients.filter(patient => patient.id !== editedPet.id)
+    setPets([unchangedPatients,editedPatient])
+  };
 
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/vets" element={<Vets pets={pets} />} />
-        <Route path="/allpatients" element={<Pets allpets={pets}/>} />
-        <Route path="/allpatients/:id" element={<PetDetails allpets={pets} onPatientDelete={deletePatient}/>} />
-        <Route path="/newpatient" element={<NewPet onAddNewPatient={addPatient} />} />
-        <Route path="/editpatient/:id" element={<EditPet allpets={pets} onEditPatient={handleEdit}/>} />
-        <Route path="/" element ={<Home allpets={pets}/>} />
+        <Route path="/vets" element={<Vets patients={patients} />} />
+        <Route path="/allpatients" element={<Patients allpatients={patients}/>} />
+        <Route path="/allpatients/:id" element={<PatientDetails allpatients={patients} onPatientDelete={deletePatient}/>} />
+        <Route path="/newpatient" element={<NewPatient onAddNewPatient={addPatient} />} />
+        <Route path="/editpatient/:id" element={<EditPatient allpatients={patients} onEditPatient={handleEdit}/>} />
+        <Route path="/" element ={<Home allpatients={patients}/>} />
       </Routes>
     </div>
   );
