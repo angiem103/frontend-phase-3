e1def81671cf91ba00b428ef6b0b6f065bd8803d
angie <amaticorena103@hotmail.com>
2022-07-28 14:08:56 -0400

    editing const

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Search from "./Search"
 
-function Home({allpets}) {
+function Home({allpatients}) {
     return (
         <div>
         <h1 className='header'>Hello!</h1>
         <p id='sub-header'>Which patient are we working on today?</p>
-        <Search pets={allpets}/>
+        <Search patients={allpatients}/>
         </div>
     )
 };
